b22721dbb16797ba38366909a928249f705216fa
6sensesrepository <6sensesrepository@gmail.com>
2025-05-05 20:00:31 +0200

    Add files via upload

---
+++ batch_files/signalYmain_phases_smoothed_conditional.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#SBATCH -p spcom                  # Partition to submit to
+SBATCH --mem=5G                  # Max CPU Memory
+#SBATCH --gres=gpu:1
+matlab -batch SignalYmain_phases_smoothed_conditional
